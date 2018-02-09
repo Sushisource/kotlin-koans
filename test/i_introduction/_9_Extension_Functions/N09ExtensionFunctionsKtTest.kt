7d6d32e589bae69aaa0ffbe80346680afd03c6ac
Spencer Judge <sushisource@gmail.com>
2018-02-09 11:16:28 -0800

    Done examples 1

--- test/i_introduction/_9_Extension_Functions/N09ExtensionFunctionsKtTest.kt
+++ test/i_introduction/_9_Extension_Functions/N09ExtensionFunctionsKtTest.kt
@@ -4,11 +4,20 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class N09ExtensionFunctionsKtTest {
-    @Test fun testIntExtension() {
+    @Test
+    fun testIntExtension() {
         assertEquals("Rational number creation error: ", RationalNumber(4, 1), 4.r())
     }
 
-    @Test fun testPairExtension() {
+    @Test
+    fun testPairExtension() {
         assertEquals("Rational number creation error: ", RationalNumber(2, 3), Pair(2, 3).r())
+    }
+
+    @Test
+    fun testBoth() {
+        assertEquals("Rational number creation error: ", RationalNumber(4, 1), rCaller(4, Int::r))
+        assertEquals("Rational number creation error: ", RationalNumber(2, 3),
+                     rCaller(Pair(2, 3), Pair<Int, Int>::r))
     }
 }
