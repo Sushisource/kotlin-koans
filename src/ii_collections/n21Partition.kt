bb60d51f19160ef8a47fa374dda772096ed24195
Spencer Judge <sushisource@gmail.com>
2018-03-09 11:19:21 -0800

    Koans part 2

--- src/ii_collections/n21Partition.kt
+++ src/ii_collections/n21Partition.kt
@@ -12,5 +12,8 @@ fun example8() {
 
 fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
     // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
+    return this.customers.filter {
+        val (deliv, undeliv) = it.orders.partition { it.isDelivered }
+        (undeliv.size - deliv.size) > 0
+    }.toSet()
 }
