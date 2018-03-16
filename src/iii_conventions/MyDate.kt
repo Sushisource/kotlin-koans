0925b0f90544fb4fdd6fa617f92b8d5b032544fa
Spencer Judge <sushisource@gmail.com>
2018-03-16 11:11:43 -0700

    Pt 3/4

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,26 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        val yc = year.compareTo(other.year)
+        if (yc != 0) return yc
+        val mc = month.compareTo(other.month)
+        if (mc != 0) return mc
+        return dayOfMonth.compareTo(other.dayOfMonth)
+    }
+}
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate {
+    return this.addTimeIntervals(timeInterval, 1)
+}
+
+operator fun MyDate.plus(rti: RepeatedTimeInterval): MyDate {
+    return this.addTimeIntervals(rti.ti, rti.n)
+}
+
+class RepeatedTimeInterval(val ti: TimeInterval, val n: Int)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +28,25 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+operator fun TimeInterval.times(other: Int) = RepeatedTimeInterval(this, other)
+
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object : Iterator<MyDate> {
+            var curD = start
+            override fun next(): MyDate {
+                val t = curD
+                curD = curD.nextDay()
+                return t
+            }
+
+            override fun hasNext(): Boolean {
+                return curD <= endInclusive
+            }
+        }
+    }
+
+    operator fun contains(d: MyDate): Boolean {
+        return d in start..endInclusive
+    }
+}
--- src/iv_properties/n33LazyProperty.kt
+++ src/iv_properties/n33LazyProperty.kt
@@ -3,11 +3,17 @@ package iv_properties
 import util.TODO
 
 class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int = todoTask33()
+    var _l = 0
+    val lazy: Int
+        get() {
+            if(_l != 0) return _l
+            _l = initializer()
+            return _l
+        }
 }
 
 fun todoTask33(): Nothing = TODO(
-    """
+        """
         Task 33.
         Add a custom getter to make the 'lazy' val really lazy.
         It should be initialized by the invocation of 'initializer()'
@@ -15,5 +21,5 @@ fun todoTask33(): Nothing = TODO(
         You can add as many additional properties as you need.
         Do not use delegated properties yet!
     """,
-    references = { LazyProperty({ 42 }).lazy }
+        references = { LazyProperty({ 42 }).lazy }
 )
